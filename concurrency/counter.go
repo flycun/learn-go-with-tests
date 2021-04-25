bda70cac5bdbb8992aa42de31f18d77d81e186e9
flycun <flycun@163.com>
2021-04-25 23:39:34 +0800

    sync.mutex wapper

--- concurrency/counter.go
+++ concurrency/counter.go
@@ -38,3 +38,35 @@ func CounterSafe() int {
 	wg.Wait()
 	return count
 }
+
+func CounterSafeWrapper() uint64 {
+	var counter Counter
+	var wg sync.WaitGroup
+	wg.Add(10)
+	for i := 0; i < 10; i++ {
+		go func() {
+			defer wg.Done()
+			for j := 0; j < 100000; j++ {
+				counter.Incr()
+			}
+		}()
+	}
+	wg.Wait()
+	return counter.Count()
+}
+
+type Counter struct {
+	mu    sync.Mutex
+	count uint64
+}
+
+func (c *Counter) Incr() {
+	c.mu.Lock()
+	c.count++
+	c.mu.Unlock()
+}
+func (c *Counter) Count() uint64 {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	return c.count
+}
--- concurrency/counter_test.go
+++ concurrency/counter_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestCounterNoSafe(t *testing.T) {
 
 	count := CounterNoSafe()
-	t.Logf("result=%d",count)
+	t.Logf("result=%d", count)
 	expected := 1_000_000
 	if count == expected {
 		t.Errorf("result epected not eq 1_000_000, count=%d", count)
@@ -15,7 +15,7 @@ func TestCounterNoSafe(t *testing.T) {
 func TestCounterSafe(t *testing.T) {
 
 	count := CounterSafe()
-	t.Logf("result=%d",count)
+	t.Logf("result=%d", count)
 	expected := 1_000_000
 	if count != expected {
 		t.Errorf("result not epected 1_000_000, count=%d", count)
@@ -23,4 +23,13 @@ func TestCounterSafe(t *testing.T) {
 
 }
 
+func TestCounterSafeWrapper(t *testing.T) {
 
+	count := CounterSafeWrapper()
+	t.Logf("result=%d", count)
+	var expected uint64 = 1_000_000
+	if count != expected {
+		t.Errorf("result not epected 1_000_000, count=%d", count)
+	}
+
+}
