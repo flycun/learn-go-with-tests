5778a50ac5ca838998992626e15798c3f5712873
flycun <flycun@163.com>
2021-04-29 00:32:53 +0800

    append

--- arrays/sum.go
+++ arrays/sum.go
@@ -1,11 +1,32 @@
 package arrays
 
-
-// Sum calculates the total from an array of numbers.
+// Sum calculates the total from a slice of numbers.
 func Sum(numbers []int) int {
 	sum := 0
-	for _, v := range numbers {
-		sum+=v
+	for _, number := range numbers {
+		sum += number
 	}
 	return sum
+}
+
+// SumAll calculates the respective sums of every slice passed in.
+func SumAll1(numbersToSum ...[]int) []int {
+	lengthOfNumbers := len(numbersToSum)
+	sums := make([]int, lengthOfNumbers)
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+
+	return sums
+}
+
+// SumAll calculates the respective sums of every slice passed in.
+func SumAll(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
 }
--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,20 +1,16 @@
 package arrays
 
 import (
+	"reflect"
 	"testing"
 )
 
-func TestSum(t *testing.T) {
+func TestSumAll(t *testing.T) {
 
-    t.Run("collection of any size", func(t *testing.T) {
-        numbers := []int{1, 2, 3}
+	got := SumAll([]int{1, 2}, []int{0, 9})
+	want := []int{3, 9}
 
-        got := Sum(numbers)
-        want := 6
-
-        if got != want {
-            t.Errorf("got %d want %d given, %v", got, want, numbers)
-        }
-    })
-
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
 }
