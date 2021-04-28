70c23547706547f87aacffe250689cfdc7c573f6
flycun <flycun@163.com>
2021-04-29 00:14:46 +0800

    add slice

--- arrays/sum.go
+++ arrays/sum.go
@@ -2,7 +2,7 @@ package arrays
 
 
 // Sum calculates the total from an array of numbers.
-func Sum(numbers [5]int) int {
+func Sum(numbers []int) int {
 	sum := 0
 	for _, v := range numbers {
 		sum+=v
