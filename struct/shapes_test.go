b49df827c5cbd429a71a51ad3e3d7803a942896d
flycun <flycun@163.com>
2021-07-04 12:03:13 +0800

    add method

--- struct/shapes_test.go
+++ struct/shapes_test.go
@@ -3,18 +3,28 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	rectangle:= Rectangle{10.0,10.0}
+	rectangle := Rectangle{10.0, 10.0}
 	got := Perimeter(rectangle)
 	want := 40.0
-	if(got!= want) {
-		t.Errorf("got %.2f want %.2f",got,want)
-	}
-}
-func TestArea(t *testing.T) {
-	rectangle:= Rectangle{10.0,10.0}
-	got := Area(rectangle)
-	want := 100.0
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
 	}
+}
+func TestArea(t *testing.T) {
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 10.0}
+		got := rectangle.Area()
+		want := 100.0
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
+	t.Run("circles", func(t *testing.T) {
+		circle:=Circle{10}
+		got := circle.Area()
+		want := 314.1592653589793
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	})
 }
