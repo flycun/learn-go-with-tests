03990ac98f0bfc70f581b7130ab946b06a0d2966
flycun <flycun@163.com>
2021-07-04 11:45:08 +0800

    add struct rectangle

--- struct/shapes_test.go
+++ struct/shapes_test.go
@@ -3,14 +3,16 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-	got := Perimeter(10.0, 10.0)
+	rectangle:= Rectangle{10.0,10.0}
+	got := Perimeter(rectangle)
 	want := 40.0
 	if(got!= want) {
 		t.Errorf("got %.2f want %.2f",got,want)
 	}
 }
 func TestArea(t *testing.T) {
-	got := Area(10.0, 10.0)
+	rectangle:= Rectangle{10.0,10.0}
+	got := Area(rectangle)
 	want := 100.0
 	if got != want {
 		t.Errorf("got %.2f want %.2f", got, want)
