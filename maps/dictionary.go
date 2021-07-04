19c951c2d039901908f98536ed1ab6d7b156a4f2
flycun <flycun@163.com>
2021-07-04 15:19:58 +0800

    add map test

---
+++ maps/dictionary.go
@@ -0,0 +1,11 @@
+package maps
+
+// Dictionary store definitions to words.
+type Dictionary map[string]string
+
+// Search find a word in the dictionary.
+func (d Dictionary) Search(word string) string {
+	return d[word]
+}
+
+
