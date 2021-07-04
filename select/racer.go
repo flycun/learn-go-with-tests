0d60369a3128d8a5d1527da67c89511361b0391e
flycun <flycun@163.com>
2021-07-04 23:29:43 +0800

    select

--- select/racer.go
+++ select/racer.go
@@ -1,23 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
 
-func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
-		return a
+var tenSecondTimeout = 10 * time.Second
+// Racer compares the response times of a and b, returning the fastest one.
+func Racer(a, b string) (winner string, error error) {
+	return ConfigurableRacer(a,b,tenSecondTimeout)
+}
+func ConfigurableRacer(a, b string, timeout time.Duration) (winner string, error error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(timeout):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
 	}
-
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	return time.Since(start)
+func ping(url string) chan bool {
+	ch := make(chan bool)
+	go func() {
+		http.Get(url)
+		ch <- true
+	}()
+	return ch
 }
