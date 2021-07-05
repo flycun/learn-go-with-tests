f70540eb23817cac2e77f7bbbac5e0c52095781f
flycun <flycun@163.com>
2021-07-05 14:32:54 +0800

    kind

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,5 +1,17 @@
 package main
 
+import "reflect"
+
 func walk(x interface{}, fn func(input string)) {
-	fn("I still can't believe South Korea beat Germany 2-0 to put them last in their group")
+	val := reflect.ValueOf(x)
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		if field.Kind() == reflect.String {
+			fn(field.String())
+		}
+
+		if field.Kind() == reflect.Struct {
+			walk(field.Interface(), fn)
+		}
+	}
 }
