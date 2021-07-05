35800582b100a1fd8b89a3d90ae864817d667f5d
flycun <flycun@163.com>
2021-07-05 23:42:15 +0800

    post

--- server/server.go
+++ server/server.go
@@ -13,7 +13,7 @@ type StubPlayerStore struct {
 }
 
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
-	score:=s.scores[name]
+	score := s.scores[name]
 	return score
 }
 
@@ -21,12 +21,25 @@ type PlayerServer struct {
 	store PlayerStore
 }
 
-
 func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		p.processWin(w)
+	case http.MethodGet:
+		p.showScore(w, r)
+	}
+
+}
+
+func (p *PlayerServer) processWin(w http.ResponseWriter) {
+	w.WriteHeader(http.StatusAccepted)
+}
+
+func (p *PlayerServer) showScore(w http.ResponseWriter, r *http.Request) {
 	player := r.URL.Path[len("/players/"):]
-	score:=p.store.GetPlayerScore(player)
-	if score==0 {
+	score := p.store.GetPlayerScore(player)
+	if score == 0 {
 		w.WriteHeader(http.StatusNotFound)
 	}
-	fmt.Fprint(w,score)
+	fmt.Fprint(w, score)
 }
