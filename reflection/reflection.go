2152920b146b6ee7682212f874ff8393059ea77c
flycun <flycun@163.com>
2021-07-05 15:00:15 +0800

    chan and func

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -1,37 +1,49 @@
 package main
 
-import "reflect"
+import (
+	"reflect"
+)
 
 func walk(x interface{}, fn func(input string)) {
 	val := getValue(x)
 
-	numberOfValues := 0
-	var getField func(int) reflect.Value
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
 
 	switch val.Kind() {
 	case reflect.String:
 		fn(val.String())
 	case reflect.Struct:
-		numberOfValues = val.NumField()
-		getField = val.Field
-	case reflect.Slice,reflect.Array:
-		numberOfValues = val.Len()
-		getField = val.Index
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
 	case reflect.Map:
 		for _, key := range val.MapKeys() {
-			walk(val.MapIndex(key).Interface(), fn)
+			walkValue(val.MapIndex(key))
 		}
-	}
-
-	for i:=0; i< numberOfValues; i++ {
-		walk(getField(i).Interface(), fn)
+	case reflect.Chan:
+		for v, ok := val.Recv(); ok; v, ok = val.Recv() {
+			walk(v.Interface(), fn)
+		}
+	case reflect.Func:
+		valFnResult := val.Call(nil)
+		for _, res := range valFnResult {
+			walk(res.Interface(), fn)
+		}
 	}
 }
 
 func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
+
 	if val.Kind() == reflect.Ptr {
 		val = val.Elem()
 	}
+
 	return val
 }
--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -4,28 +4,17 @@ import (
 	"reflect"
 	"testing"
 )
-type Person struct {
-	Name    string
-	Profile Profile
-}
-
-type Profile struct {
-	Age  int
-	City string
-}
 
 func TestWalk(t *testing.T) {
 
-	cases := []struct{
-		Name string
-		Input interface{}
+	cases := []struct {
+		Name          string
+		Input         interface{}
 		ExpectedCalls []string
-	} {
+	}{
 		{
 			"Struct with one string field",
-			struct {
-				Name string
-			}{ "Chris"},
+			struct{ Name string }{"Chris"},
 			[]string{"Chris"},
 		},
 		{
@@ -62,7 +51,7 @@ func TestWalk(t *testing.T) {
 		},
 		{
 			"Slices",
-			[]Profile {
+			[]Profile{
 				{33, "London"},
 				{34, "Reykjavík"},
 			},
@@ -70,19 +59,11 @@ func TestWalk(t *testing.T) {
 		},
 		{
 			"Arrays",
-			[2]Profile {
+			[2]Profile{
 				{33, "London"},
 				{34, "Reykjavík"},
 			},
 			[]string{"London", "Reykjavík"},
-		},
-		{
-			"Maps",
-			map[string]string{
-				"Foo": "Bar",
-				"Baz": "Boz",
-			},
-			[]string{"Bar", "Boz"},
 		},
 	}
 
@@ -98,6 +79,79 @@ func TestWalk(t *testing.T) {
 			}
 		})
 	}
+
+	t.Run("with maps", func(t *testing.T) {
+		aMap := map[string]string{
+			"Foo": "Bar",
+			"Baz": "Boz",
+		}
+
+		var got []string
+		walk(aMap, func(input string) {
+			got = append(got, input)
+		})
+
+		assertContains(t, got, "Bar")
+		assertContains(t, got, "Boz")
+	})
+
+	t.Run("with channels", func(t *testing.T) {
+		aChannel := make(chan Profile)
+
+		go func() {
+			aChannel <- Profile{33, "Berlin"}
+			aChannel <- Profile{34, "Katowice"}
+			close(aChannel)
+		}()
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aChannel, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("with function", func(t *testing.T) {
+		aFunction := func() (Profile, Profile) {
+			return Profile{33, "Berlin"}, Profile{34, "Katowice"}
+		}
+
+		var got []string
+		want := []string{"Berlin", "Katowice"}
+
+		walk(aFunction, func(input string) {
+			got = append(got, input)
+		})
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
 }
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
 
+type Profile struct {
+	Age  int
+	City string
+}
+
+func assertContains(t *testing.T, haystack []string, needle string) {
+	contains := false
+	for _, x := range haystack {
+		if x == needle {
+			contains = true
+		}
+	}
+	if !contains {
+		t.Errorf("expected %+v to contain %q but it didn't", haystack, needle)
+	}
+}
