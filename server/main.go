b19337e3abcfd4a0c24f069d3ca751db75cc0c95
flycun <flycun@163.com>
2021-07-06 00:19:59 +0800

    refator

--- server/main.go
+++ server/main.go
@@ -4,21 +4,7 @@ import (
 	"log"
 	"net/http"
 )
-func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{map[string]int{}}
-}
 
-type InMemoryPlayerStore struct{
-	store map[string]int
-}
-
-func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.store[name]++
-}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return i.store[name]
-}
 
 func main() {
 	server:=&PlayerServer{NewInMemoryPlayerStore()}
