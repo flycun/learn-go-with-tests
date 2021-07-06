c4e38cca6b45db8bf2a3ec7479ca94d145f5082e
flycun <flycun@163.com>
2021-07-06 16:02:38 +0800

    嵌入

--- json/server.go
+++ json/server.go
@@ -14,24 +14,24 @@ type PlayerStore interface {
 // PlayerServer is a HTTP interface for player information.
 type PlayerServer struct {
 	store PlayerStore
-	router *http.ServeMux
+	http.Handler
 }
 
 func NewPlayerServer(store PlayerStore) *PlayerServer {
-	p := &PlayerServer{
-		store,
-		http.NewServeMux(),
-	}
+	p := new(PlayerServer)
+	p.store=store
 
-	p.router.Handle("/league", http.HandlerFunc(p.leagueHandler))
-	p.router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	router:=http.NewServeMux()
 
+	router.Handle("/league", http.HandlerFunc(p.leagueHandler))
+	router.Handle("/players/", http.HandlerFunc(p.playersHandler))
+	p.Handler=router
 	return p
 }
 
-func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	p.router.ServeHTTP(w,r)
-}
+//func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+//	p.ServeHTTP(w,r)
+//}
 
 func (p *PlayerServer) leagueHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
