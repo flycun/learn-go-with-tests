8d030ae8c3e92c007e29f8a595ac8da6219e262d
flycun <flycun@163.com>
2021-07-08 20:19:01 +0800

    init command line

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -30,6 +30,22 @@ func (c *ConfigurableSleeper) Sleep() {
 	time.Sleep(c.duration)
 }
 
+type CountdownOperationsSpy struct {
+	Calls []string
+}
+
+func (s *CountdownOperationsSpy) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *CountdownOperationsSpy) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
 // Countdown prints a countdown from 3 to out.
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
